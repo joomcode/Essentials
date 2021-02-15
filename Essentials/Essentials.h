c2d70c8bc506ceddf097d3dd01eba2da5ee26713
Andrey Morozov <morozov@joom.com>
2021-02-15 16:22:56 +0300

    Unite headers

---
+++ Essentials/Essentials.h
@@ -0,0 +1,27 @@
+//
+//  Essentials.h
+//  Essentials
+//
+//  Created by Nick Tymchenko on 19/08/15.
+//  Copyright (c) 2015 Nick Tymchenko. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Essentials.
+FOUNDATION_EXPORT double EssentialsVersionNumber;
+
+//! Project version string for Essentials.
+FOUNDATION_EXPORT const unsigned char EssentialsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Essentials/PublicHeader.h>
+
+#import <Essentials/NNScrollViewWrapper.h>
+#import <Essentials/NNKeyboardListener.h>
+#import <Essentials/UIScrollView+NNKeyboardListener.h>
+#import <Essentials/NNLayoutWrapper.h>
+#import <Essentials/UIView+NNSystemLayoutSize.h>
+#import <Essentials/UIViewController+NNLayoutGuide.h>
+#import <Essentials/NNTouchInterceptorView.h>
+#import <Essentials/NSObject+NNProxy.h>
+#import <Essentials/NNLayoutSpacer.h>
--- Essentials/Fancy/Fancy.h
+++
@@ -1,9 +0,0 @@
-//
-//  Fancy.h
-//  Essentials
-//
-//  Created by Nick Tymchenko on 24/08/15.
-//  Copyright (c) 2015 Nick Tymchenko. All rights reserved.
-//
-
-#import "NSObject+NNProxy.h"
